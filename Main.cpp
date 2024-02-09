c4cb704274df0f232598683603112efdef33d345
domoco <minoooow39@gmail.com>
2024-02-09 13:01:26 +0900

    more example

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include "Grid.h"
 #include <array>
+#include <iostream>
 
 int main()
 {
@@ -9,8 +10,23 @@ int main()
 		0, 4, 6,
 		7, 5, 8
 	};
-	Puzzle::Grid puzzle{ initial, Puzzle::Heuristic::ManhattanDistance, Puzzle::GoalState::Ordered };
+
+	Puzzle::Grid puzzle{ initial, Puzzle::Heuristic::HammingDistance, Puzzle::GoalState::Ordered };
 	puzzle.SolveBoard();
 	puzzle.Print();
+
+	std::cout << "\n---\n\n";
+
+	std::array<int, 9> anotherInitial
+	{
+		0, 1, 2,
+		8, 4, 3,
+		7, 6, 5
+	};
+
+	Puzzle::Grid anotherPuzzle{ anotherInitial, Puzzle::Heuristic::ManhattanDistance, Puzzle::GoalState::Clockwise };
+	anotherPuzzle.SolveBoard();
+	anotherPuzzle.Print();
+
 	return 0;
 }
